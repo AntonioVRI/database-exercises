5a9ca6d16850742a1e0d5751caa2d97dea6ce3af
Anthony V <avidaurri4@gmail.com>
2021-01-11 11:54:41 -0600

    creating tables transactions & items

--- testMigration.sql
+++ testMigration.sql
@@ -1,1 +1,16 @@
 use codeup_test_db;
+
+creat table if not exists transactions(
+    id int,
+    date DATE,
+    description varchar(255),
+    amount decimal(10, 2)
+);
+
+creat table if not exists items(
+    id int,
+    price decimal(10, 2),
+    description varchar(255),
+    name varchar(100),
+    category varchar(150)
+);
