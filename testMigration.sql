ff086571a3c51ce36a745a4eb1d6022d0ebe8e5e
Anthony V <avidaurri4@gmail.com>
2021-01-11 12:08:13 -0600

    testMigration practicing w tables

--- testMigration.sql
+++ testMigration.sql
@@ -14,3 +14,21 @@ creat table if not exists items(
     name varchar(100),
     category varchar(150)
 );
+
+create table if not exists users(
+    id int unsigned not null auto_increment,
+    is_admin tinyint default 0,
+    zipcode char(15) default '77777',
+    primary key(id)
+);
+
+create table if not exists contacts(
+    id int unsigned not null auto_increment,
+    first_name varchar(50),
+    last_name varchar(50),
+    phone_number varchar(50) not null,
+    address varchar(150),
+    email varchar(50),
+    zipcode char(20),
+    primary key(id)
+);
