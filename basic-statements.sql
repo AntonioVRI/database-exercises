fe12843848570c1744cca9a7d5c9d269f418537f
Anthony V <avidaurri4@gmail.com>
2021-01-13 09:22:26 -0600

    basic-statements

--- basic-statements.sql
+++ basic-statements.sql
@@ -1,14 +1,22 @@
 use codeup_test_db;
 
-    INSERT into albums(artist, name) VALUES ('Metallica', 'Death Magnetic');
-    INSERT into albums(artist, name) VALUES ('Kid Cudi', 'Man on the Moon: The End of Day');
-    INSERT into albums(artist, name) VALUES ('Pantera', 'Vulgar Display of Power');
-    INSERT into albums(artist, name) VALUES ('Kanye West', 'Graduation');
-    INSERT into albums(artist, name) VALUES ('AC/DC', 'High Voltage');
+SELECT 'Books written 2001 - Present' AS '';
+SELECT title, author_last_name
+FROM books
+WHERE year >= 2001;
 
-    INSERT INTO albums(artist, name)
-    VALUES ('Drake', 'So Far Gone'),
-           ('Tame Impala', 'Lonerism'),
-           ('Mike Jones', 'The American Dream'),
-           ('Deftones', 'Around the Fur'),
-           ('Freddie Gibbs', 'Pinata');
+SELECT 'First record in the books table' AS '';
+SELECT title
+FROM books
+WHERE id = 1;
+
+UPDATE books
+SET title = 'Huckleberry Finn'
+WHERE id <= 5;
+
+SELECT 'First record in the books table' AS '';
+SELECT title
+FROM books
+WHERE id <= 5;
+
+DELETE FROM books WHERE title = 'Huckleberry Finn';
