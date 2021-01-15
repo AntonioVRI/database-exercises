e0a7ce6c708af32f6ca6088147d75cf3927c8e1c
Anthony V <avidaurri4@gmail.com>
2021-01-15 14:01:09 -0600

    joins_exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,5 +1,8 @@
 USE join_test_db;
 
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS roles;
+
 CREATE TABLE roles(
    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
    name VARCHAR(100) NOT NULL,
@@ -27,3 +30,40 @@ INSERT INTO users (name, email, role_id) VALUES
 ('adam', 'adam@example.com', 3),
 ('jane', 'jane@example.com', null),
 ('mike', 'mike@example.com', null);
+
+select * from roles;
+select * from users;
+
+# Insert 4 new users into the database. One should have a NULL role. The other three should be authors.
+INSERT INTO users (name, email, role_id) VALUES
+('eric', 'eric@example.com', 2),
+('anthony', 'anthony@example.com', 2),
+('sarah', 'sarah@example.com', null),
+('tim', 'tim@example.com', 2);
+
+select * from users;
+
+# Use join, left join, and right join to combine results from the users and roles tables as we did in the lesson. Before you run each query, guess the expected number of results.
+# INNER
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+         JOIN roles ON users.role_id = roles.id;
+
+# LEFT
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+         LEFT JOIN roles ON users.role_id = roles.id;
+
+# RIGHT
+SELECT users.name AS  user_name, roles.name AS role_name
+FROM users
+         RIGHT JOIN roles ON users.role_id = roles.id;
+
+# Using the example in the Associative Table Joins section as a guide, write a query that shows each department along
+# with the name of the current manager for that department.
+SELECT r.name, COUNT(*) as Users
+FROM users as u
+         JOIN roles as r
+              ON u.role_id = r.id
+GROUP BY r.id;
+
